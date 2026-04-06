83b75cfd18fa2c76b9720a440bc6aff2b0c9f5ca
gbhbkarunarathne <gbhbkarunarathne@students.nsbm.ac.lk>
2026-04-06 09:26:54 +0530

    Add files via upload

---
+++ 111.py
@@ -0,0 +1,7 @@
+num=10
+if num == 0:
+    print("zero")
+elif num > 0:
+    print("positive number")
+else:
+    print("negative number")
---
+++ 222.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter num1"))
+num2 = int(input("enter num2"))
+
+if num1 > num2:
+    print("num1 large number")
+else:
+    print("num2 large number")
