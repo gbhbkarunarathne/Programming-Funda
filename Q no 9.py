f1841f380e3009c8ad36f7d22f768f863213acc1
gbhbkarunarathne <gbhbkarunarathne@students.nsbm.ac.lk>
2026-04-21 16:19:37 +0530

    Add files via upload

---
+++ Q no 9.py
@@ -0,0 +1,11 @@
+while true:
+    num=int(input("enter num or -1 to stop"))
+
+    if num == -1:
+        break
+
+    if num%2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+        
